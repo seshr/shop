680db16e3e09d5f18a838120f9f0b437311f52ae
Serhii <sshr2018git@gmail.com>
2019-03-22 13:33:40 +0200

    Updates

---
+++ precache-manifest.01d4e430c27c44f7f2b6106931496f2c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3a4c97e913adeac98b80",
+    "url": "/shop/static/js/runtime~main.3a4c97e9.js"
+  },
+  {
+    "revision": "2e15dbc12b013cc705a3",
+    "url": "/shop/static/js/main.2e15dbc1.chunk.js"
+  },
+  {
+    "revision": "ead9b09dba710b7d30fa",
+    "url": "/shop/static/js/1.ead9b09d.chunk.js"
+  },
+  {
+    "revision": "2e15dbc12b013cc705a3",
+    "url": "/shop/static/css/main.43be1790.chunk.css"
+  },
+  {
+    "revision": "c3d43b4e4c5f526f633de6df3ec2e74f",
+    "url": "/shop/index.html"
+  }
+];
